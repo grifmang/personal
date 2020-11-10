9864331714677ef42e46f860f306cbccb68468f3
Tim Griffith <grifmang@gmail.com>
2020-11-10 14:08:30 -0500

    meta tag server add

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const path = require('path');
+const fs = require('fs')
+
+app.get('/', function(request, response) {
+//   console.log('Home page visited!');
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  fs.readFile(filePath, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    const result = data.replace(/\$OG_IMAGE/g, '%PUBLIC_URL%/port.png');
+    response.send(result);
+  });
+});
