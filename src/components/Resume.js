76d5bc9edde234d6332cb7827e1f997977ec2ab9
Tim Griffith <grifmang@gmail.com>
2024-11-26 19:43:52 -0500

    resume change

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
             <div className="section-heading">
                 <h3 className="title is-2">Resume</h3>
                 <h4 className="subtitle is-5">Click below to download my resume</h4>
-                <a href="https://timgriffith.us/TimGriffithResume.pdf" className="button is-link is-medium">
+                <a href="https://drive.google.com/file/d/1HaU6Nwu0ecuKBcE8pCMEcXG3H6DH03qv/view?usp=sharing" className="button is-link is-medium">
                     <span className="icon">
                     <i className="fas fa-file-alt"></i>
                     </span>
